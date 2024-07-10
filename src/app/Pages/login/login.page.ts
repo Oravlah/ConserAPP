ecd8a0fa41b91541cd876f45e355c70346b5b46a
Oravlah <oravla.hhh@gmail.com>
2024-07-10 15:08:22 -0400

    Se agregaron validaciones dentro de la page login, en los diferentes campos existentes.

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  nombre: string = '';
+  password: string = '';
+  nombreErrors: string = '';
+  passwordErrors: string = '';
+  aceptoTerminos: boolean = false;
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private alertController: AlertController) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  validarNombre() {
+    this.nombreErrors = '';
+    if (!this.nombre) {
+      this.nombreErrors = '• Debe ingresar un nombre de usuario o email.';
+    } else if ((this.nombre.includes('@') && (this.nombre.length < 6 || this.nombre.length > 35)) || (!this.nombre.includes('@') && (this.nombre.length < 4 || this.nombre.length > 15))) {
+      this.nombreErrors = this.nombre.includes('@') ? '• El email debe tener entre 6 y 35 caracteres.' : '• El nombre de usuario debe tener entre 4 y 15 caracteres.';
+    } else {
+      this.usuarioService.getUsuarios().subscribe(usuarios => {
+        if (!usuarios.some(u => u.nombre === this.nombre || u.email === this.nombre)) {
+          this.nombreErrors = '• El nombre de usuario o email no es válido.';
+        }
+      });
+    }
   }
 
+  validarPassword() {
+    this.passwordErrors = '';
+    if (!this.password) {
+      this.passwordErrors = '• Debe ingresar una contraseña.';
+    } else if (this.password.length < 6 || this.password.length > 18) {
+      this.passwordErrors = '• La contraseña debe tener entre 6 y 18 caracteres.';
+    }
+  }
+
+  validarLogin() {
+    this.validarNombre();
+    this.validarPassword();
+
+    if (this.nombreErrors || this.passwordErrors) {
+      return; // Si hay errores, no procedemos con el login
+    }
+
+    this.usuarioService.getUsuarios().subscribe(usuarios => {
+      const usuarioValido = usuarios.find(u => (u.nombre === this.nombre || u.email === this.nombre) && u.contraseña === this.password);
+      if (usuarioValido) {
+        this.presentAlert('Éxito', 'Inicio de sesión correcto.');
+      } else {
+        this.presentAlert('Error', 'El nombre de usuario, email o contraseña no son correctos.');
+      }
+    });
+  }
+
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
